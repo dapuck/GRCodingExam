c65c25aca71cd1133d62ab7c2c7ff7f148851f98
Ian McCall <imccall@da-puck.com>
2016-09-20 21:45:39 -0500

    Modified test to look at content.

--- tests/File-test.js
+++ tests/File-test.js
@@ -5,31 +5,64 @@ const File = require("../src/File");
 const assert = require("assert");
 
 const numTestLines = 5;
+const expectedRows = [
+  {
+    lastname: "McCall",
+    firstname: "Ian",
+    gender: "M",
+    favoritecolor: "red",
+    dateofbirth: "9/21/1986"
+  },{
+    lastname: "Doe",
+    firstname: "Jane",
+    gender: "F",
+    favoritecolor: "blue",
+    dateofbirth: "8/16/1984"
+  },{
+    lastname: "Lovelace",
+    firstname: "Ada",
+    gender: "F",
+    favoritecolor: "green",
+    dateofbirth: "12/10/1815"
+  },{
+    lastname: "Wozniak",
+    firstname: "Steve",
+    gender: "M",
+    favoritecolor: "yellow",
+    dateofbirth: "8/11/1950"
+  },{
+    lastname: "Gates",
+    firstname: "William",
+    gender: "M",
+    favoritecolor: "beige",
+    dateofbirth: "10/28/1955"
+  }
+];
 
 describe("FileTest", () => {
   it("Initialize file.", () => {
     const file = new File();
-    assert.strictEqual(file.rows.length, 0);
+    assert.deepEqual(file.rows, []);
   });
   it("Load a pipe delimited file.", () => {
     const file = new File();
     return file.load("./tests/data/test-data-pipe.txt","|")
     .then(() => {
-      assert.strictEqual(file.rows.length, numTestLines);
+      assert.deepEqual(file.rows, expectedRows);
     });
   });
   it("Load a pipe comma file.", () => {
     const file = new File();
     return file.load("./tests/data/test-data-comma.txt",",")
     .then(() => {
-      assert.strictEqual(file.rows.length, numTestLines);
+      assert.deepEqual(file.rows, expectedRows);
     });
   });
   it("Load a pipe space file.", () => {
     const file = new File();
     return file.load("./tests/data/test-data-space.txt"," ")
     .then(() => {
-      assert.strictEqual(file.rows.length, numTestLines);
+      assert.deepEqual(file.rows, expectedRows);
     });
   });
 });
