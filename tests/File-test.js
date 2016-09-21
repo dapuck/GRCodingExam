c473d12287b43203e9e365960e91ca0259aecfc2
Ian McCall <imccall@da-puck.com>
2016-09-20 22:07:33 -0500

    New block of tests for sorting

--- tests/File-test.js
+++ tests/File-test.js
@@ -38,8 +38,41 @@ const expectedRows = [
     dateofbirth: "10/28/1955"
   }
 ];
+const expectedRowsSortByGender = [
+  {
+    lastname: "Doe",
+    firstname: "Jane",
+    gender: "F",
+    favoritecolor: "blue",
+    dateofbirth: "8/16/1984"
+  },{
+    lastname: "Lovelace",
+    firstname: "Ada",
+    gender: "F",
+    favoritecolor: "green",
+    dateofbirth: "12/10/1815"
+  },{
+    lastname: "Gates",
+    firstname: "William",
+    gender: "M",
+    favoritecolor: "beige",
+    dateofbirth: "10/28/1955"
+  },{
+    lastname: "McCall",
+    firstname: "Ian",
+    gender: "M",
+    favoritecolor: "red",
+    dateofbirth: "9/21/1986"
+  },{
+    lastname: "Wozniak",
+    firstname: "Steve",
+    gender: "M",
+    favoritecolor: "yellow",
+    dateofbirth: "8/11/1950"
+  }
+];
 
-describe("FileTest", () => {
+describe("File Load Test", () => {
   it("Initialize file.", () => {
     const file = new File();
     assert.deepEqual(file.rows, []);
@@ -64,5 +97,20 @@ describe("FileTest", () => {
     .then(() => {
       assert.deepEqual(file.rows, expectedRows);
     });
+  });
+});
+
+describe("File Sort Test", () => {
+  const file = new File();
+  
+  before(() => {
+    return file.load("./tests/data/test-data-pipe.txt","|");
+  });
+  it("Sort by Gender.", () => {
+    assert.deepEqual(file.sortBy("gender"),expectedRowsSortByGender);
+  });
+  it("Sort by Birth Date.", () => {
+  });
+  it("Sort by Last Name, descending.", () => {
   });
 });
