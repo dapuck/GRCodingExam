9c822b198e635a02d26cb6ee13146416637dc2e8
Ian McCall <imccall@da-puck.com>
2016-09-21 21:34:54 -0500

    More work on CLI, per step 1.

--- index.js
+++ index.js
@@ -5,27 +5,22 @@ const File = require("./src/File");
 const path = require("path");
 const options = require("nomnom")
   .options({
-    "delimiter": {
-      abbr: "d",
-      default: "|",
-      transform: (delim) => {
-        switch(delim) {
-          case "pipe":
-            return "|";
-          case "comma":
-            return ",";
-          case "space":
-            return " ";
-        }
+    "file1": {
+      position: 0,
+      required: true,
+      transform: (filename) => {
+        return path.resolve(filename);
       }
     },
-    "order": {
-      abbr: "o",
-      default: "gender",
-      choices: ["gender","dob","name"]
+    "file2": {
+      position: 1,
+      required: true,
+      transform: (filename) => {
+        return path.resolve(filename);
+      }
     },
-    "file": {
-      position: 0,
+    "file3": {
+      position: 2,
       required: true,
       transform: (filename) => {
         return path.resolve(filename);
@@ -35,7 +30,32 @@ const options = require("nomnom")
   .parse();
 
 const file = new File();
-file.load(options.file,options.delimiter)
+file.load(options.file1,"|")
 .then(() => {
+  return file.load(options.file2,",");
+})
+.then(() => {
+  return file.load(options.file3," ");
+})
+.then(() => {
+  const byGender = file.sortBy("gender");
+  console.log("Sorted by gender:");
+  console.log(`LastName | FirstName | Gender | FavoriteColor | DateOfBirth`);
+  byGender.forEach((row) => {
+    console.log(`${row.lastname} | ${row.firstname} | ${row.gender} | ${row.favoritecolor} | ${row.dateofbirth}`);
+  });
   
+  const byBirthDate = file.sortBy("birthdate");
+  console.log("Sorted by birth date:");
+  console.log(`LastName | FirstName | Gender | FavoriteColor | DateOfBirth`);
+  byBirthDate.forEach((row) => {
+    console.log(`${row.lastname} | ${row.firstname} | ${row.gender} | ${row.favoritecolor} | ${row.dateofbirth}`);
+  });
+  
+  const byLastName = file.sortBy("lastname");
+  console.log("Sorted by last name, descending:");
+  console.log(`LastName | FirstName | Gender | FavoriteColor | DateOfBirth`);
+  byLastName.forEach((row) => {
+    console.log(`${row.lastname} | ${row.firstname} | ${row.gender} | ${row.favoritecolor} | ${row.dateofbirth}`);
+  });
 });
