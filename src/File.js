9f88cb4e511be05580bbf810f798fdc4260db256
Ian McCall <imccall@da-puck.com>
2016-09-21 20:26:41 -0500

    Implement sorting.

--- src/File.js
+++ src/File.js
@@ -31,8 +31,42 @@ class File {
     });
   }
   
-  sortBy() {
-    
+  sortBy(field) {
+    field = field.toLowerCase();
+    switch(field) {
+      case "gender":
+        return this._sortByGender();
+      case "birthdate":
+        return this._sortByBirthDate();
+      case "lastname":
+        return this._sortByLastName();
+    }
+  }
+  
+  _sortByGender() {
+    return this.rows.sort((a,b) => {
+      if(a.gender === 'F' && b.gender === 'M') {
+        return -1;
+      }
+      if(a.gender === 'M' && b.gender === 'F') {
+        return 1;
+      }
+      return a.lastname.localeCompare(b.lastname);
+    });
+  }
+  
+  _sortByBirthDate() {
+    return this.rows.sort((a,b) => {
+      let da = a.dateofbirth.match(/(\d{1,2})\/(\d{1,2})\/(\d{4})/);
+      let db = b.dateofbirth.match(/(\d{1,2})\/(\d{1,2})\/(\d{4})/);
+      return `${da[3]}${da[1]}${da[2]}`.localeCompare(`${db[3]}${db[1]}${db[2]}`);
+    });
+  }
+  
+  _sortByLastName() {
+    return this.rows.sort((a,b) => {
+      return a.lastname.localeCompare(b.lastname) * -1;
+    });
   }
 }
 
