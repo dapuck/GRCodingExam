dfda4c322ff8246cfdcd4b4c6be9e2a2d3a7c199
Ian McCall <imccall@da-puck.com>
2016-09-22 22:30:07 -0500

    Comment out console log. Convert "name" to "lastname".

--- app.js
+++ app.js
@@ -14,14 +14,18 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.post('/records', (req, res) => {
   let delim = req.body.delimiter;
   let row = req.body.row;
-  console.log(`row: ${row}, delimiter: ${delim}`);
+//  console.log(`row: ${row}, delimiter: ${delim}`);
   file.addRow(row,delim);
 //  console.log(file.rows.length);
   res.send({status:"ok", count: file.rows.length});
 });
 
 app.get('/records/:field', (req, res) => {
-  let data = file.sortBy(req.params.field);
+  let field = req.params.field.toLowerCase();
+  if(field === "name") {
+    field = "lastname";
+  }
+  let data = file.sortBy(field);
 //  console.log(data);
   res.send(data);
 });
